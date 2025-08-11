cafa3253d96bf7933dd1f172142478df3ae568cd
Paul Zarudnev <rockinpaulz@gmail.com>
2025-08-11 17:44:01 +0200

    feat: add death animation with slow-motion and collision removal

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,11 +4,15 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died!")
+	# Slow down the game speed.
 	Engine.time_scale = 0.5
+	# Remove collision shape of body so the player falls down.
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 	
 
 
 func _on_timer_timeout() -> void:
+	# Restore the game speed.
 	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
