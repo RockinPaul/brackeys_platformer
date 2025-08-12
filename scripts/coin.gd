0a872d1f95ad3273fea0d45d84a5be27a4cdc28a
Paul Zarudnev <rockinpaulz@gmail.com>
2025-08-12 15:16:13 +0200

    feat: add coin pickup animation and sound effects

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,20 +1,9 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("Coin!")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-	
-
 @onready var game_manager: Node = %GameManager
-
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	game_manager.add_point()
 	print("+1 coin!")
-	queue_free()
+	animation_player.play("pickup")
